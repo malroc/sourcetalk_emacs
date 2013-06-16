f2d5ad8b2568eaeaa3bf2b7230f960415de9c2a6
Oleg Kalistratov <oleg.kalistratov@gmail.com>
2013-06-16 22:54:58 +0400

    Initial commit

---
+++ sourcetalk.el
@@ -0,0 +1,48 @@
+;;; sourcetalk.el --- SourceTalk (http://sourcetalk.net) plugin for Emacs
+
+;; Copyright (C) 2013 Oleg Kalistratov
+
+;; Author: Oleg Kalistratov <oleg@sourcetalk.net>
+;; Keywords: sourcetalk code discussion
+;; Version: 0.0.1
+
+(require 'request)
+
+(defun sourcetalk-get-current-line ()
+  "Returns the currently selected line number (starting from 1)"
+  (save-restriction
+    (widen)
+    (save-excursion
+      (beginning-of-line)
+      (1+ (count-lines 1 (point))))))
+
+(defun sourcetalk-get-buffer-content ()
+  "Returns the text content of the current buffer"
+  (substring-no-properties (buffer-string)))
+
+(defun sourcetalk-get-buffer-file-name ()
+  "Returns the file name of the current buffer"
+  (buffer-file-name (current-buffer)))
+
+(defun sourcetalk-start-external-conference ()
+  "Starts a new SourceTalk conference in a browser window"
+  (interactive)
+  (setq test-cont (test-content))
+  (request
+   "http://sourcetalk.net/conferences.json"
+   :type "POST"
+   :data `(("conference[file_name]" . ,(sourcetalk-get-buffer-file-name))
+           ("conference[source]" . ,(sourcetalk-get-buffer-content)))
+   :parser 'json-read
+   :success (function*
+             (lambda
+               (&key data &allow-other-keys)
+               (message (concat "http://sourcetalk.net/conferences/"
+                                   (assoc-default 'slug data)
+                                   "/"
+                                   (number-to-string
+                                    (sourcetalk-get-current-line))))))))
+
+(global-set-key (kbd "C-c C-s") 'sourcetalk-start-external-conference)
+
+(provide 'sourcetalk)
